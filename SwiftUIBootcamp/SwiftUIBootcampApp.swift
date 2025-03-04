58b39ca1e08cb2a73d8014e681f521371cf599d7
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2025-03-04 15:27:11 +0000

    Project setup

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Claire Roughan on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIBootcampTests/SwiftUIBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampTests.swift
+//  SwiftUIBootcampTests
+//
+//  Created by Claire Roughan on 04/03/2025.
+//
+
+import Testing
+@testable import SwiftUIBootcamp
+
+struct SwiftUIBootcampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
